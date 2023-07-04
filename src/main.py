0a64f87ccc2b0a0f9b7a52f44be7c2d37db23854
TagRomEdu <qwertystyles@gmail.com>
2023-07-04 11:01:50 +0500

    fixup! updated data.py with new list

--- src/main.py
+++ src/main.py
@@ -1,16 +1,19 @@
 from random import choice
-from data import answer_list, sweet_list, another_list
+from data import answer_list, sweet_list, another_list, agreed_list
 
 
 def main():
+
     hello_name = input("Привет Мир, я магический шар, и я знаю ответ на любой твой вопрос!"
                        "\nКак твое имя, Божье творение?\n")
     print(f"Привет, {hello_name}!")
+
     while True:
         question = input(f"{choice(another_list)}\n")
         print(choice(answer_list))
         another_question = input(f"Хочешь ещё что-нибудь спросить, {choice(sweet_list)}?\n")
-        if another_question.lower() != 'да':
+
+        if another_question.lower() not in agreed_list:
             print("Давай, проваливай! Всего хорошего.")
             break
 
